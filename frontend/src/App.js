ad3c92530d06b242fdb75a6028f46625835d7c8d
lhh.Nam <lehoanghuynam0401@gmail.com>
2021-03-08 13:22:23 +0700

    add homePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,90 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ListCategories from './components/atom/admin/categories/ListCategories';
-import EditCategories from './components/atom/admin/categories/EditCategories';
-import Login from './components/atom/admin/auth/Login';
+import ListCategories from "./components/atom/admin/categories/ListCategories";
+import EditCategories from "./components/atom/admin/categories/EditCategories";
+import Login from "./components/atom/admin/auth/Login";
 import ListAdmins from "./components/atom/admin/users/ListAdmins";
 import CreateAdmin from "./components/atom/admin/users/CreateAdmin";
-import './App.css';
-import { BrowserRouter as Router, Route, withRouter } from "react-router-dom"
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Product from "./components/atom/admin/Products/Product";
-import { Switch, Redirect } from 'react-router-dom';
-import Home from './components/atom/admin/home/Home';
-import CreateProduct from './components/atom/admin/Products/CreateProduct';
+import { Switch } from "react-router-dom";
+import Home from "./components/atom/admin/home/Home";
+import CreateProduct from "./components/atom/admin/Products/CreateProduct";
 
-import EditProduct from './components/atom/admin/Products/EditProduct';
+import EditProduct from "./components/atom/admin/Products/EditProduct";
 import ProtectedRoute from "./helpers/admin/checkRolesAuth";
-import EditCurrentAdmin from './components/atom/admin/users/EditCurrentAdmin';
-import Banners from './components/atom/admin/banners/Banners';
-import CreateBanner from './components/atom/admin/banners/CreateBanner';
+import EditCurrentAdmin from "./components/atom/admin/users/EditCurrentAdmin";
+import Banners from "./components/atom/admin/banners/Banners";
+import CreateBanner from "./components/atom/admin/banners/CreateBanner";
 
+// Client
+import "./styles/App.scss";
+import HomePage from "./pages/HomePage";
+import Header from "./components/atom/user/header/Header";
+import Footer from "./components/atom/user/footer/Footer";
 
 export default class App extends Component {
-
   render() {
-
     return (
       <Router>
-
         <Switch>
+          {/* ----------- Client ----------- */}
+
+          <Route path="/">
+            <Header />
+            <HomePage />
+            <Footer />
+          </Route>
+
+          {/* -----x----- Client -----x----- */}
+
+          {/* ----------- Admin ----------- */}
+
           <Route path="/login" component={Login} />
 
-          <ProtectedRoute
-            path="/products"
-            component={Product}
-          />
-          <ProtectedRoute
-            exact path="/"
-            component={Home}
-          />
+          <ProtectedRoute path="/products" component={Product} />
+          <ProtectedRoute exact path="/" component={Home} />
 
-          <ProtectedRoute
-            path="/product/create"
-            component={CreateProduct}
-          />
+          <ProtectedRoute path="/product/create" component={CreateProduct} />
 
-          <ProtectedRoute
-            path="/editproduct/:id"
-            component={EditProduct}
-          />
+          <ProtectedRoute path="/editproduct/:id" component={EditProduct} />
 
-          <ProtectedRoute
-            path="/categories"
-            component={ListCategories}
-          />
+          <ProtectedRoute path="/categories" component={ListCategories} />
           <ProtectedRoute
             path="/editcategory/:categoryId"
             component={EditCategories}
           />
 
-          <ProtectedRoute
-          exact
-            path="/users"
-            component={ListAdmins}
-          />
+          <ProtectedRoute exact path="/users" component={ListAdmins} />
 
-          <ProtectedRoute
-            path="/listadmins/create"
-            component={CreateAdmin}
-          />
-          <ProtectedRoute          
-            path="/editusers/:id"
-            component={EditCurrentAdmin}
-          />
-          <ProtectedRoute
-          exact
-            path="/admin/banners"
-            component={Banners}
-          />
+          <ProtectedRoute path="/listadmins/create" component={CreateAdmin} />
+          <ProtectedRoute path="/editusers/:id" component={EditCurrentAdmin} />
+          <ProtectedRoute exact path="/admin/banners" component={Banners} />
           <ProtectedRoute
             path="/admin/banners/create"
             component={CreateBanner}
           />
+
+          {/* -----x----- Admin -----x----- */}
         </Switch>
       </Router>
-    )
+    );
   }
 }
-
---
+++ frontend/src/components/atom/user/header/Header.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+//Images
+import Logo from "../../../../assets/images/logo.png";
+import Vietnamese from "../../../../assets/images/en_US.png";
+import English from "../../../../assets/images/vi_VN.png";
+
+//Icons
+import {
+  IconBasket,
+  IconNews,
+  IconSearch,
+  IconSupport,
+} from "../../../../assets/icons";
+
+function Header() {
+  const [focus, setFocus] = useState(false);
+
+  const onSearch = () => {
+    setFocus(true);
+  };
+
+  return (
+    <header>
+      <div className="menu">
+        <div className="menu--top">
+          <div className="menu--top__container">
+            <div className="menu--top--left">
+              <div className="m-tl-item language">
+                <p className="language--title">Chọn ngôn ngữ</p>
+
+                <p className="language--img">
+                  <img src={Vietnamese} alt="" />
+                </p>
+
+                <p className="language--img">
+                  <img src={English} alt="" />
+                </p>
+              </div>
+
+              {/* <div className="m-tl-item">
+                <a>
+                  <p>Tin tức</p>
+                  <IconNews className="header-icon icon-mtl" />
+                </a>
+              </div> */}
+
+              <div className="m-tl-item">
+                <a>
+                  <p>Hỗ trợ khách hàng</p>
+                  <IconSupport className="header-icon icon-mtl" />
+                </a>
+              </div>
+            </div>
+
+            <div className="menu--top--search">
+              <div className="menu--top--search__container">
+                <input type="text" placeholder="Tìm kiếm" onChange={onSearch} />
+                {/* <IconSearch className="header-icon phd-search" /> */}
+              </div>
+            </div>
+
+            <div className="menu--top--right">
+              <a>
+                <IconBasket className="header-icon icon-mtr" />
+                <p>Mua Hàng</p>
+              </a>
+
+              <a>
+                <p>Đăng kí</p>
+              </a>
+
+              <a>
+                <p>Đăng nhập</p>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="menu--bottom">
+          <ul>
+            <li className="cate-item">Trang chủ</li>
+            <li className="cate-item">Giới thiệu</li>
+            <li className="cate-item">Sản phẩm</li>
+            <li className="cate-item">Hoạt động</li>
+
+            <li className="menu-logo">
+              <img src={Logo} alt="" />
+            </li>
+
+            <li className="cate-item">DV nông nghiệp</li>
+            <li className="cate-item">R&D</li>
+            <li className="cate-item">Liên hệ</li>
+            <li className="cate-item">Tuyển dụng</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
